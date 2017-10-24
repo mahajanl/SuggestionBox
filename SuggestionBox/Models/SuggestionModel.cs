0c233d41a6456b5b2a3ebeb523f53f53dce5e21e
mahajanl <lakshamima@gmail.com>
2017-10-24 11:29:53 -0400

    created some scaffolding and an index

---
+++ SuggestionBox/Models/SuggestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuggestionBox.Models
+{
+    public class SuggestionModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Topic { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
